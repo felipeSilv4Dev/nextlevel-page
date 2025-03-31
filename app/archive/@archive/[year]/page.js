417d45ed8752f21e7d05cee186d0536b1caf7d74
felipeSilv4Dev <felipesilva.developer@gmail.com>
2025-03-31 10:52:17 -0300

    adding routes and errors

--- app/archive/@archive/[year]/page.js
+++
@@ -1,8 +0,0 @@
-import NewsList from "@/component/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilterNewsPage({ params }) {
-  const newsYear = params.year;
-  const news = getNewsForYear(newsYear);
-  return <NewsList news={news} />;
-}
